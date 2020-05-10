4e700f27fce4207644bf67ff2f1240af0668564e
ronaldod <31585599+ronaldod@users.noreply.github.com>
2020-05-10 14:59:14 +0200

    updated to 8.8
    The file was still with settings from an older version

--- configuration.php
+++ configuration.php
@@ -1,9 +1,25 @@
 <?php
+$global['configurationVersion'] = 2;
 $global['disableAdvancedConfigurations'] = 0;
 $global['videoStorageLimitMinutes'] = 0;
-$global['webSiteRootURL'] = 'PROTOCOL://DOMAIN/';
+if(!empty($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME']!=='localhost' && !filter_var($_SERVER['SERVER_NAME'], FILTER_VALIDATE_IP)) {
+    // get the subdirectory, if exists
+    $subDir = str_replace(array($_SERVER["DOCUMENT_ROOT"], 'videos/configuration.php'), array('',''), __FILE__);
+    $global['webSiteRootURL'] = "http".(!empty($_SERVER['HTTPS'])?"s":"")."://".$_SERVER['SERVER_NAME'].$subDir;
+}else{
+    $global['webSiteRootURL'] = 'PROTOCOL://DOMAIN/';
+}
+$global['disableAdvancedConfigurations'] = 0;
+$global['videoStorageLimitMinutes'] = 0;
 $global['systemRootPath'] = '/var/www/localhost/htdocs/';
 $global['salt'] = 'SALT';
+$global['disableTimeFix'] = 0;
+$global['enableDDOSprotection'] = 1;
+$global['ddosMaxConnections'] = 40;
+$global['ddosSecondTimeout'] = 5;
+$global['strictDDOSprotection'] = 0;
+$global['noDebug'] = 0;
+
 
 $mysqlHost = 'DB_HOST';
 $mysqlPort = '3306';
